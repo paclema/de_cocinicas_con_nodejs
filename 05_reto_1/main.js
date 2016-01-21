b50d0b111b649ecc14742672dbb4eec7e5a29aa2
Pablo Clemente <paclema@gmail.com>
2016-01-21 10:50:29 +0100

    05_reto_1 solved

--- 05_reto_1/main.js
+++ 05_reto_1/main.js
@@ -1,1 +1,12 @@
-fs = require('fs');
+var fs = require('fs'),
+    readline = require('readline');
+
+var rd = readline.createInterface({
+    input: fs.createReadStream('comida.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+rd.on('line', function(line) {
+    console.log(line + " me mola");
+});
