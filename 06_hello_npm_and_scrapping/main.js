4e1d4d1a371ac1ed50e8cc3b3751ea1ae3bcabf8
Pablo Clemente <paclema@gmail.com>
2016-01-21 12:00:06 +0100

    triying to improve 06 example

--- 06_hello_npm_and_scrapping/main.js
+++ 06_hello_npm_and_scrapping/main.js
@@ -6,18 +6,25 @@ var request = require("request"),
   cheerio = require("cheerio"),
   url = "http://www.meneame.net";
 
-setInterval(getLastNews, 2000);
+setInterval(getLastNews, 200);
 
 function getLastNews () {
   request(url, function (error, response, body) {
     if (!error) {
       var $ = cheerio.load(body),
+        /*
         //nombre y primer voto
         primera_noticia = $("#newswrap .news-summary")[0];
         var votos = $(primera_noticia).find(".votes").text().split(" ")[1];
         var nombre = $(primera_noticia).find("h2").text();
 
         console.log(nombre + " --> " + votos);
+        */
+
+        primera_noticia = $("#newswrap .interaction-count")[3];
+        var votos = $(primera_noticia).text()
+        console.log(votos);
+
 
     } else {
       console.log("error: " + error);
